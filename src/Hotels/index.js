5e23ae03b515bb6a1b078ede20759270f8e8ed6d
darling_d <ddarling212@gmail.com>
2021-12-06 20:40:46 -0500

    Rendered data model

--- src/Hotels/index.js
+++ src/Hotels/index.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Hotel from "./Hotel.js";
 
-const renderHotel = () => null;
+const renderHotel = (hotel) => <Hotel key={hotel.id} hotel={hotel} />;
 
 const HotelsList = (props) => {
   if (!props.hotels) {
     return null;
   }
-
-  return <ul className="hotels-list">{}</ul>;
+  const hotelListElements = props.hotels.map(renderHotel);
+  return <ul className="hotels-list">{hotelListElements}</ul>;
 };
 
 export default HotelsList;
